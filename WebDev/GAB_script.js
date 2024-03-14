e55c048aa25e6c379913247fb114bf9144ffb426
Gabrax <gab-oz@wp.pl>
2024-03-14 19:12:28 +0100

    progress

--- WebDev/GAB_script.js
+++ WebDev/GAB_script.js
@@ -1,4 +1,45 @@
 
+
+var heading = document.getElementById('Curriculum');
+var newText = heading.innerHTML.replace('C', '<span style="-webkit-text-fill-color: transparent; -webkit-text-stroke: 1px #ef5f5f;">C</span>');
+heading.innerHTML = newText;
+
+var heading = document.getElementById('Curriculum');
+var newText = heading.innerHTML.replace('V', '<span style="-webkit-text-fill-color: transparent; -webkit-text-stroke: 1px #ef5f5f;">V</span>');
+  heading.innerHTML = newText;
+
+
+
+
   var audio = document.getElementById("_Audio");
   audio.volume = 0.01;
+  
+  document.querySelector('.Volumebtn').addEventListener('click', function() {
+    var audio = document.getElementById("_Audio");
+    
+    if (audio.paused) {
+      
+      audio.play();
+      playButton.textContent = "Pause";
+  } else {
+      
+      audio.pause();
+      playButton.textContent = "Play";
+  }
+
+
+    console.log('Button clicked!');
+  });
+
+  var volumeImage = document.getElementById("volImg");
+  volumeImage.addEventListener("click", function() {
+    
+    if (volumeImage.src.endsWith("Res/volON.png")) {
+        
+        volumeImage.src = "Res/volOFF.png";
+    } else {
+        
+        volumeImage.src = "Res/volON.png";
+    }
+});
 

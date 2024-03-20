f937d5f2aedbd01e8df079c30dae76256812be1a
Gabrax <gab-oz@wp.pl>
2024-03-20 21:51:16 +0100

    needs new html structure

--- WebDev/GAB_script.js
+++ WebDev/GAB_script.js
@@ -31,11 +31,25 @@
           }, 10); // Small delay to allow class removal to take effect
       });
   });
-  
+
 
   function scrolltoTop(){
     window.scrollTo({top: 0, behavior: 'smooth'});
   }
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      console.log(entry)
+      if(entry.isIntersecting){
+        entry.target.classList.add('show');
+      }else{
+        entry.target.classList.remove('show');
+      }
+    });
+  });
+
+  const hiddenElements = document.querySelector('.hidden');
+  hiddenElements.forEach((el) => observer.observe(el));
 
 
   /*var audio = document.getElementById("_Audio");
